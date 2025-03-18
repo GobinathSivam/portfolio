707af21cc5d5a90114060703a4e446d6b02398d0
GobinathSivam <sivamgobinath@gmail.com>
2025-03-18 20:46:18 +0530

    Few JS functionality have modified

--- js/main.js
+++ js/main.js
@@ -179,3 +179,12 @@ function sendMail() {
 	window.location.href = link;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+	const loader = document.getElementById("loader");
+	const content = document.getElementById("content");
+
+	setTimeout(() => {
+		loader.style.display = "none";
+		content.style.display = "block";
+	}, 2000); // Adjust the time as per the loading duration
+});
